bd5bfb965bfe938d84974f770bf799a9a92cc327
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-07-31 22:40:35 -0400

    Scaffold for Order

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801023855) do
+ActiveRecord::Schema.define(version: 20150801024013) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "order_id"
     t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
