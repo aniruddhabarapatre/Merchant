d4847d61989c7e5819142e328aca423869820a3b
aniruddhabarapatre <aniruddhabarapatre@gmail.com>
2015-07-31 22:49:51 -0400

    Creating data models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
+  has_many :order_items
+
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates_numericality_of :price
+
+  def price=(input)
+    input.delete!("$")
+    super
+  end
 end
